c9e2d9dcd83ce7b77fa1a72ca02393d12b7782a2
Theo Cheron <theo.chrn@gmail.com>
2023-06-28 09:01:14 +0200

    Update Bills.js

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -35,6 +35,7 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1) 
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+      j
     })
   })
 })
